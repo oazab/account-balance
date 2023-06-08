16dae2092cf87265dc1ed0fb1c2aad0975804acb
oazab <120120731+oazab@users.noreply.github.com>
2023-06-08 12:23:23 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+class bank:
+    id=""
+    name=""
+    balance=0
+#object 1
+acc1=bank()
+acc1.id="91919"
+acc1.name="ahmed"
+acc1.balance=5000
+#object 2
+acc2=bank()
+acc2=id="2222"
+acc2.name="Ayman"
+acc2.balance=2000
